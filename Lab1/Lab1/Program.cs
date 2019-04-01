5cd95ef81642f6a5a70cedf157cb29cf35059540
Shatashelans <shatashelans@gmail.com>
2019-04-01 17:01:26 +0200

    The first and the second labs

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Card
+    {
+        private string bookTitle;      //the title of the book
+        private string bookAuthor;     //the author of the book
+        private int booksOut;          //the general num of books out
+        private DateTime dateOut;      //the date of giving the book to the client
+        private DateTime dateIn;       //the date of bringing back the book
+
+        /// <summary>
+        /// Constructor by default
+        /// </summary>
+        public Card()
+        {
+            bookTitle = "Deafualt";
+            bookAuthor = "Default";
+            booksOut = 0;
+            dateOut = new DateTime(1970, 1, 1);     //the common default time
+            dateIn = new DateTime(1000, 1, 1);      //nullable time for the current program
+        }
+
+        /// <summary>
+        /// Constructor for the input data
+        /// </summary>
+        /// <param name="title">Title of the book</param>
+        /// <param name="author">Author of the book</param>
+        /// <param name="outs">The number of books out</param>
+        /// <param name="dOut">The date the client brought out the book</param>
+        /// <param name="dIn">The date (if it exists) the client brought book back</param>
+        public Card(string title, string author, int outs, DateTime dOut, DateTime dIn)
+        {
+            bookTitle = title;
+            bookAuthor = author;
+            booksOut = outs;
+            dateOut = dOut;
+
+            if (dIn > dOut)                                                             //if the day of bringig back was earlier than out, the back is nullabe
+                dateIn = dIn;
+            else
+                dateIn = DateTime.Parse("1/1/1000");
+        }
+
+        /// <summary>
+        /// Calculates the number of days between out and in
+        /// </summary>
+        /// <returns>The number of days between bringing out and bringing back</returns>
+        public int NumOfDaysCalculation()                                               
+        {
+            if (dateIn != DateTime.Parse("1/1/1000"))
+                return (int)(dateIn - dateOut).TotalDays;
+            else
+                return -1;
+        }
+
+        /// <summary>
+        /// Output of the general info about the book
+        /// </summary>
+        public void Output()
+        {
+            Console.WriteLine("The title of the book: {0}", bookTitle);
+            Console.WriteLine("The author of the book: {0}", bookAuthor);
+            Console.WriteLine("The number of books out: {0}", booksOut);
+            Console.WriteLine("The date of bringing out: {0}", dateOut);
+            if (dateIn != DateTime.Parse("1/1/1000"))
+            {
+                Console.WriteLine("The date of bringing in: {0}", dateIn);
+                Console.WriteLine("The number of days the client used the book: {0}", NumOfDaysCalculation());  //using of the method "NumOfDaysCalculation()"
+            }
+            else
+            {
+                Console.WriteLine("The date of bringing in: the book is not at the library yet!");
+            }
+            Console.WriteLine();
+            Console.WriteLine("=======================================================================================");
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// If the title and the author of two books are similar, the number of books out for the current objects sums with the other
+        /// </summary>
+        /// <param name="card">The second card you want to process for the sum</param>
+        public void Sum (Card card)                                                                             
+        {
+            if (bookAuthor == card.bookAuthor && bookTitle == card.bookTitle)
+            {
+                booksOut += card.booksOut;
+            }
+        }
+
+        /// <summary>
+        /// Class destructor
+        /// </summary>
+        ~Card()
+        {
+            bookTitle = null;
+            bookAuthor = null;
+            booksOut = 0;
+            dateOut = DateTime.Parse("1/1/1000");
+            dateIn = DateTime.Parse("1/1/1000");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card card1 = new Card();
+            Card card2 = new Card("Odyssey of captain Blood", "Rafael Sabatini", 2, DateTime.Parse("5/1/2019"), DateTime.Parse("10/2/2019"));   // Two books with the same
+            Card card3 = new Card("Odyssey of captain Blood", "Rafael Sabatini", 5, DateTime.Parse("5/1/2017"), DateTime.Parse("1/2/2017"));    // author and title,
+            card2.Sum(card3);                                                                                                                   // that's why we can process them on the Sum() function
+            card1.Sum(card2);                                                                                                                   // But here we have the different ones, so will be nothing
+            Card card4 = new Card("War and piece", "Lev Tolstoy", 25, DateTime.Parse("10/1/2019"), DateTime.Parse("25/2/2017"));                // The date of in is earlier than out, so it will be "1/1/1000" (nullable)
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// // Output
+            card1.Output();
+            card2.Output();
+            card3.Output();
+            card4.Output();
+        }
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Vector
+    {
+        private float[] arr;                        // The vector we processing
+
+        /// <summary>
+        /// Constructor by default
+        /// </summary>
+        public Vector()
+        {
+            arr = new float[0];
+        }
+
+        /// <summary>
+        /// Random constructor by the length
+        /// </summary>
+        /// <param name="size">The length of the random array</param>
+        public Vector(int size)
+        {
+            arr = new float[size];
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = (float)(100 - random.NextDouble() * 200);
+            }
+        }
+
+        /// <summary>
+        /// The constructor by values
+        /// </summary>
+        /// <param name="x">The array we want to copy</param>
+        public Vector(float[] x)
+        {
+            arr = new float[x.Length];
+            for (int i = 0; i < x.Length; i++)
+            {
+                arr[i] = x[i];
+            }
+        }
+
+        /// <summary>
+        /// Changes positions of two negative numbers which are situated in succession
+        /// </summary>
+        public void ChangePositionOfNegativeNumbers()
+        {
+            int position = -1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (position == -1 && arr[i] < 0)
+                {
+                    position = i;
+                }
+                else if (position != -1 && arr[i] < 0)
+                {
+                    arr[i] += arr[position];
+                    arr[position] = arr[i] - arr[position];
+                    arr[i] -= arr[position];
+                    position = -1;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Searches for the maximum and the minimum value of the array
+        /// </summary>
+        /// <returns>The array, where the first number is the maximum value and the second one is the minimum</returns>
+        public float[] FindMaxAndMin()
+        {
+            float[] maxMin = { 0, 0 };
+            foreach (var number in arr)
+            {
+                if (number > maxMin[0])
+                {
+                    maxMin[0] = number;
+                }
+                else if (number < maxMin[1])
+                {
+                    maxMin[1] = number;
+                }
+            }
+            return maxMin;
+        }
+
+        /// <summary>
+        /// Searches for the position of the maximum and the minumum values in the array
+        /// </summary>
+        /// <param name="maxMin">The values of the maximum and the minimum in the current array</param>
+        /// <returns>The array of positions of the maximum and the minimum of current array</returns>
+        public int[] PositionOfMaxAndMin(float[] maxMin)
+        {
+            int[] mMPositions = { -1, -1 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == maxMin[0])
+                {
+                    mMPositions[0] = i + 1;
+                }
+                else if (arr[i] == maxMin[1])
+                {
+                    mMPositions[1] = i + 1;
+                }
+            }
+            return mMPositions;
+        }
+
+        /// <summary>
+        /// Performs output and uses all fumction before except constructors
+        /// </summary>
+        public void Output()
+        {
+            Console.WriteLine("The primary array: ");
+            foreach (var number in arr)
+            {
+                Console.Write("{0}; ", number);
+            }
+            float[] maxMin = FindMaxAndMin();
+            int[] positionMaxMin = PositionOfMaxAndMin(maxMin);
+            Console.WriteLine("\n\rThe maximum and the minimum of this array:\n\rMAX: {0}[{1}], MIN: {2}[{3}]", maxMin[0], positionMaxMin[0], maxMin[1], positionMaxMin[1]);
+
+            ChangePositionOfNegativeNumbers();
+            Console.WriteLine("The new array: ");
+            foreach (var number in arr)
+            {
+                Console.Write("{0}; ", number);
+            }
+            Console.WriteLine();
+            positionMaxMin = PositionOfMaxAndMin(maxMin);
+            Console.WriteLine("Positions of maximum and minumum in the new array are:\n\rMAX: {0}, MIN: {1}\n\r", positionMaxMin[0], positionMaxMin[1]);
+            Console.WriteLine("========================================================================================================================");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector();
+            float[] vs = { 1, -4, 5, -7, 25, -32, 44, 56, 100, -200 };
+            Vector v2 = new Vector(vs);
+            v1.Output();
+            v2.Output();
+        }
+    }
+}
