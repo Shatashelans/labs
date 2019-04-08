37914640dcadca17ece1e6f0238640691be31a80
Shatashelans <shatashelans@gmail.com>
2019-04-08 22:13:33 +0200

    Added a big num of comments for better understanding

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -4,26 +4,29 @@ namespace Lab1
 {
     class Card
     {
-        private string bookTitle;      //the title of the book
-        private string bookAuthor;     //the author of the book
-        private int booksOut;          //the general num of books out
-        private DateTime dateOut;      //the date of giving the book to the client
-        private DateTime dateIn;       //the date of bringing back the book
+        private string bookTitle;      // the title of the book
+        private string bookAuthor;     // the author of the book
+        private int booksOut;          // the general num of books out
+        private DateTime dateOut;      // the date of giving the book to the client
+        private DateTime dateIn;       // the date of bringing back the book
 
         /// <summary>
-        /// Constructor by default
+        /// Constructor by default. We use it in case we have no parametrs.
+        /// Places in the tags of title and author value "Deafault", value of books out of the library is 0,
+        /// the initial date for the book out is "initial point" ('01.01.1970') and the end date is the nullable year for this application
+        /// (because DateTime can't has the nullable value)
         /// </summary>
         public Card()
         {
-            bookTitle = "Deafualt";
-            bookAuthor = "Default";
-            booksOut = 0;
-            dateOut = new DateTime(1970, 1, 1);     //the common default time
-            dateIn = new DateTime(1000, 1, 1);      //nullable time for the current program
+            bookTitle = "Deafualt";                 // Making title and
+            bookAuthor = "Default";                 // author defaults
+            booksOut = 0;                           // By default the num of books out is 0
+            dateOut = new DateTime(1970, 1, 1);     // the common default time
+            dateIn = new DateTime(1000, 1, 1);      // nullable time for the current program
         }
 
         /// <summary>
-        /// Constructor for the input data
+        /// Constructor for the input data (if we have some parameters in bracets while the object initialized)
         /// </summary>
         /// <param name="title">Title of the book</param>
         /// <param name="author">Author of the book</param>
@@ -32,13 +35,13 @@ namespace Lab1
         /// <param name="dIn">The date (if it exists) the client brought book back</param>
         public Card(string title, string author, int outs, DateTime dOut, DateTime dIn)
         {
-            bookTitle = title;
-            bookAuthor = author;
-            booksOut = outs;
-            dateOut = dOut;
+            bookTitle = title;                                                          // Putting the title of the book
+            bookAuthor = author;                                                        // Putting the author of the book
+            booksOut = outs;                                                            // Putting the day the book was brought out of the library
+            dateOut = dOut;                                                             // -||-                     was brought in the library
 
-            if (dIn > dOut)                                                             //if the day of bringig back was earlier than out, the back is nullabe
-                dateIn = dIn;
+            if (dIn > dOut)                                                             // If the day of bringig back was earlier than out, the back is nullabe
+                dateIn = dIn;                                                           // Else the date is like a similar parameter
             else
                 dateIn = DateTime.Parse("1/1/1000");
         }
@@ -49,10 +52,10 @@ namespace Lab1
         /// <returns>The number of days between bringing out and bringing back</returns>
         public int NumOfDaysCalculation()                                               
         {
-            if (dateIn != DateTime.Parse("1/1/1000"))
-                return (int)(dateIn - dateOut).TotalDays;
+            if (dateIn != DateTime.Parse("1/1/1000"))                                   // Checking the nullable time. If everything is OK
+                return (int)(dateIn - dateOut).TotalDays;                               // return the difference between the initial and the end date
             else
-                return -1;
+                return -1;                                                              // else it returns the mistake (-1 code)
         }
 
         /// <summary>
@@ -60,22 +63,22 @@ namespace Lab1
         /// </summary>
         public void Output()
         {
-            Console.WriteLine("The title of the book: {0}", bookTitle);
-            Console.WriteLine("The author of the book: {0}", bookAuthor);
-            Console.WriteLine("The number of books out: {0}", booksOut);
-            Console.WriteLine("The date of bringing out: {0}", dateOut);
-            if (dateIn != DateTime.Parse("1/1/1000"))
+            Console.WriteLine("The title of the book: {0}", bookTitle);                 // Output the title
+            Console.WriteLine("The author of the book: {0}", bookAuthor);               // the author
+            Console.WriteLine("The number of books out: {0}", booksOut);                // the num of books out at this moment
+            Console.WriteLine("The date of bringing out: {0}", dateOut);                // the date of bringing out
+            if (dateIn != DateTime.Parse("1/1/1000"))                                   // Checking the nullable value of the date of bringing in
             {
                 Console.WriteLine("The date of bringing in: {0}", dateIn);
                 Console.WriteLine("The number of days the client used the book: {0}", NumOfDaysCalculation());  //using of the method "NumOfDaysCalculation()"
-            }
+            }                                                                                                   // If the book hasn't been brought yet
             else
             {
-                Console.WriteLine("The date of bringing in: the book is not at the library yet!");
+                Console.WriteLine("The date of bringing in: the book is not at the library yet!");              // output contains this information
             }
             Console.WriteLine();
-            Console.WriteLine("=======================================================================================");
-            Console.WriteLine();
+            Console.WriteLine("======================================================================================="); // Splitting the output of different books for
+            Console.WriteLine();                                                                                          // the better view
         }
 
         /// <summary>
@@ -84,14 +87,15 @@ namespace Lab1
         /// <param name="card">The second card you want to process for the sum</param>
         public void Sum (Card card)                                                                             
         {
-            if (bookAuthor == card.bookAuthor && bookTitle == card.bookTitle)
-            {
-                booksOut += card.booksOut;
-            }
+            if (bookAuthor == card.bookAuthor && bookTitle == card.bookTitle)                                   // If the books are similar
+            {                                                                                                   // (by their author and title)
+                booksOut += card.booksOut;                                                                      // we add the value of books out of the second book
+            }                                                                                                   // to the first book
         }
 
         /// <summary>
         /// Class destructor
+        /// It puts the nullable values (global or only for this program) or 0 values (for the num of books were brought out)
         /// </summary>
         ~Card()
         {
@@ -106,7 +110,7 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Card card1 = new Card();
+            Card card1 = new Card();                                                                                                            // Using the constructor by default
             Card card2 = new Card("Odyssey of captain Blood", "Rafael Sabatini", 2, DateTime.Parse("5/1/2019"), DateTime.Parse("10/2/2019"));   // Two books with the same
             Card card3 = new Card("Odyssey of captain Blood", "Rafael Sabatini", 5, DateTime.Parse("5/1/2017"), DateTime.Parse("1/2/2017"));    // author and title,
             card2.Sum(card3);                                                                                                                   // that's why we can process them on the Sum() function
--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -7,11 +7,11 @@ namespace Lab2
         private float[] arr;                        // The vector we processing
 
         /// <summary>
-        /// Constructor by default
+        /// Constructor by default (if we have no parameters)
         /// </summary>
         public Vector()
         {
-            arr = new float[0];
+            arr = new float[0];                     // Initializes the vector (array) with the length equal to 0
         }
 
         /// <summary>
@@ -20,11 +20,11 @@ namespace Lab2
         /// <param name="size">The length of the random array</param>
         public Vector(int size)
         {
-            arr = new float[size];
-            Random random = new Random();
-            for (int i = 0; i < arr.Length; i++)
+            arr = new float[size];                                  // Initialize the vector by its length (by parameter)
+            Random random = new Random();                           // Using the random object
+            for (int i = 0; i < arr.Length; i++)    
             {
-                arr[i] = (float)(100 - random.NextDouble() * 200);
+                arr[i] = (float)(100 - random.NextDouble() * 200);  // creates numbers from -100 to 100
             }
         }
 
@@ -34,11 +34,11 @@ namespace Lab2
         /// <param name="x">The array we want to copy</param>
         public Vector(float[] x)
         {
-            arr = new float[x.Length];
-            for (int i = 0; i < x.Length; i++)
-            {
-                arr[i] = x[i];
-            }
+            arr = new float[x.Length];                              // Initialize the length of the vector with the help of length of the input array we want to copy
+            for (int i = 0; i < x.Length; i++)                      // Process of copying
+            {                                                       // .
+                arr[i] = x[i];                                      // .
+            }                                                       // .
         }
 
         /// <summary>
@@ -46,19 +46,19 @@ namespace Lab2
         /// </summary>
         public void ChangePositionOfNegativeNumbers()
         {
-            int position = -1;
+            int position = -1;                                      // By default we have a not existing position
             for (int i = 0; i < arr.Length; i++)
             {
-                if (position == -1 && arr[i] < 0)
-                {
-                    position = i;
+                if (position == -1 && arr[i] < 0)                   // If it's the first number of the couple
+                {                                                   // and it's lower than 0 (negative number)
+                    position = i;                                   // then we remember this position,
                 }
-                else if (position != -1 && arr[i] < 0)
-                {
-                    arr[i] += arr[position];
-                    arr[position] = arr[i] - arr[position];
-                    arr[i] -= arr[position];
-                    position = -1;
+                else if (position != -1 && arr[i] < 0)              // else if we met the the second number of the couple
+                {                                                   // and it's also negative
+                    arr[i] += arr[position];                        // then perform replacing these values:
+                    arr[position] = arr[i] - arr[position];         // .
+                    arr[i] -= arr[position];                        // .
+                    position = -1;                                  // And to find another couples we destroy the position of the first number from the couple
                 }
             }
         }
@@ -69,8 +69,8 @@ namespace Lab2
         /// <returns>The array, where the first number is the maximum value and the second one is the minimum</returns>
         public float[] FindMaxAndMin()
         {
-            float[] maxMin = { 0, 0 };
-            foreach (var number in arr)
+            float[] maxMin = { 0, 0 };                              // Initializing the array of max and min values of the vector
+            foreach (var number in arr)                             // Standart way to find the maximum and the minimum values:
             {
                 if (number > maxMin[0])
                 {
@@ -91,16 +91,16 @@ namespace Lab2
         /// <returns>The array of positions of the maximum and the minimum of current array</returns>
         public int[] PositionOfMaxAndMin(float[] maxMin)
         {
-            int[] mMPositions = { -1, -1 };
-            for (int i = 0; i < arr.Length; i++)
+            int[] mMPositions = { -1, -1 };                         // Initialize the position of minimum and maximum values (default positions are not existing)
+            for (int i = 0; i < arr.Length; i++)                    // Standart way to perform searching
             {
                 if (arr[i] == maxMin[0])
                 {
-                    mMPositions[0] = i + 1;
+                    mMPositions[0] = i + 1;                         // Adding 1 for the human visibility
                 }
                 else if (arr[i] == maxMin[1])
                 {
-                    mMPositions[1] = i + 1;
+                    mMPositions[1] = i + 1;                         // -||-
                 }
             }
             return mMPositions;
@@ -112,22 +112,22 @@ namespace Lab2
         public void Output()
         {
             Console.WriteLine("The primary array: ");
-            foreach (var number in arr)
+            foreach (var number in arr)                             // Loop outputting the values of the initial vector
             {
                 Console.Write("{0}; ", number);
             }
-            float[] maxMin = FindMaxAndMin();
-            int[] positionMaxMin = PositionOfMaxAndMin(maxMin);
+            float[] maxMin = FindMaxAndMin();                       // Find values of max and min in the vector
+            int[] positionMaxMin = PositionOfMaxAndMin(maxMin);     // and find the initial position of them
             Console.WriteLine("\n\rThe maximum and the minimum of this array:\n\rMAX: {0}[{1}], MIN: {2}[{3}]", maxMin[0], positionMaxMin[0], maxMin[1], positionMaxMin[1]);
 
-            ChangePositionOfNegativeNumbers();
+            ChangePositionOfNegativeNumbers();                      // Perform the main changes in the program: replacing negative numbers by their couples
             Console.WriteLine("The new array: ");
-            foreach (var number in arr)
+            foreach (var number in arr)                             // Output the final vector
             {
                 Console.Write("{0}; ", number);
             }
             Console.WriteLine();
-            positionMaxMin = PositionOfMaxAndMin(maxMin);
+            positionMaxMin = PositionOfMaxAndMin(maxMin);           // Find the final position of max and min in the vector
             Console.WriteLine("Positions of maximum and minumum in the new array are:\n\rMAX: {0}, MIN: {1}\n\r", positionMaxMin[0], positionMaxMin[1]);
             Console.WriteLine("========================================================================================================================");
         }
@@ -136,11 +136,11 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
-            Vector v1 = new Vector();
-            float[] vs = { 1, -4, 5, -7, 25, -32, 44, 56, 100, -200 };
-            Vector v2 = new Vector(vs);
-            v1.Output();
-            v2.Output();
+            Vector v1 = new Vector();                                       // Using of constructor by default
+            float[] vs = { 1, -4, 5, -7, 25, -32, 44, 56, 100, -200 };      
+            Vector v2 = new Vector(vs);                                     // Using of constructor by values
+            v1.Output();                                                    // Output of the results
+            v2.Output();                                                    // .
         }
     }
 }
